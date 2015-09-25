8373cc7cf778bfe1b011224f7fed1369b0efd2c2
Suresh Babu Kumpati <sureshkumpati@gmail.com>
2015-09-25 14:02:32 +0530

    Completed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
-# Matrix inversion is usually a costly computation and there may be some benefit 
-# to caching the inverse of a matrix rather than compute it repeatedly. The 
-# following two functions are used to cache the inverse of a matrix. 
+## Matrix inversion is usually a costly computation and there may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly. The 
+## following two functions are used to cache the inverse of a matrix. 
  
-# makeCacheMatrix creates a list containing a function to 
-# 1. set the value of the matrix 
-# 2. get the value of the matrix 
-# 3. set the value of inverse of the matrix 
-# 4. get the value of inverse of the matrix 
+## makeCacheMatrix creates a list containing a function to the below
+## 1. set the value of the matrix 
+## 2. get the value of the matrix 
+## 3. set the value of inverse of the matrix 
+## 4. get the value of inverse of the matrix 
  makeCacheMatrix <- function(x = matrix()) { 
      inv <- NULL 
      set <- function(y) { 
@@ -20,13 +20,13 @@
  } 
  
  
-# The following function returns the inverse of the matrix. It first checks if 
-# the inverse has already been computed. If so, it gets the result and skips the 
-# computation. If not, it computes the inverse, sets the value in the cache via 
-# setinverse function. 
+## The following function returns the inverse of the matrix. It first checks if 
+## the inverse has already been computed. If so, it gets the result and skips the 
+## computation. If not, it computes the inverse, sets the value in the cache via 
+## setinverse function. 
  
  
- # This function assumes that the matrix is always invertible. 
+## This function assumes that the matrix is always invertible. 
  cacheSolve <- function(x, ...) { 
      inv <- x$getinverse() 
      if(!is.null(inv)) { 
@@ -40,7 +40,7 @@
  } 
  
  
-## Sample run: 
+## Below Sample to obtain results: 
 ## > x = rbind(c(1, -1/4), c(-1/4, 1)) 
 ## > m = makeCacheMatrix(x) 
 ## > m$get() 
@@ -48,13 +48,11 @@
 ## [1,]  1.00 -0.25 
 ## [2,] -0.25  1.00 
  
- 
 ## No cache in the first run 
 ## > cacheSolve(m) 
 ##           [,1]      [,2] 
 ## [1,] 1.0666667 0.2666667 
 ## [2,] 0.2666667 1.0666667 
- 
  
 ## Retrieving from the cache in the second run 
 ## > cacheSolve(m) 
